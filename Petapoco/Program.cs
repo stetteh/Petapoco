aa344aef072bc22ecdbfa161c05bf8b442fa6c11
stetteh <stettehq@gmail.com>
2016-03-16 15:25:24 -0500

    insert 1st record

--- Petapoco/Program.cs
+++ Petapoco/Program.cs
@@ -10,6 +10,19 @@ namespace Petapoco
     {
         static void Main(string[] args)
         {
+            var db = new PetaPoco.Database("dbstring");
+
+            //
+            SalesPeople newSalesPeople = new SalesPeople();
+            SalesPeople newSalesPeople2 = new SalesPeople();
+
+            newSalesPeople.Name = "Seth Quaye";
+            newSalesPeople2.Name = "Deacon";
+
+            db.Insert(newSalesPeople);
+            db.Insert(newSalesPeople2);
+            //Assert.True(newSalesPeople.PeopleId != 0);
+
         }
     }
 }
--- Petapoco/SalesPeople.cs
+++ Petapoco/SalesPeople.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PetaPoco;
 
 namespace Petapoco
 {
+    [TableName("SalesPeople")]
+    [PrimaryKey("PeopleId")]
     public class SalesPeople
     {
         public int PeopleId { get; set; }
