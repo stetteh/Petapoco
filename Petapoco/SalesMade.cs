7be5c8c49145a57000be0a83305abd760a11d54f
stetteh <stettehq@gmail.com>
2016-03-16 14:24:33 -0500

    classes created

---
+++ Petapoco/SalesMade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petapoco
+{
+    public class SalesMade
+    {
+        public int SalesId { get; set; }
+        public string Name { get; set; }
+        public DateTime SalesDate { get; set; }
+        public int PreTaxAmount { get; set; }
+    }
+}
---
+++ Petapoco/SalesPeople.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petapoco
+{
+    public class SalesPeople
+    {
+        public int PeopleId { get; set; }
+        public string Name { set; get; }
+    }
+}
